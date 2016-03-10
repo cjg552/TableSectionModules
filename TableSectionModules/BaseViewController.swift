0ab31891db21a2bf91a2ab2285e26d050a44dcd4
Carlos Jimenez Galindo <iphonehipoglucido@gmail.com>
2016-03-10 17:50:12 +0100

    New function to replace a module by other

--- TableSectionModules/BaseViewController.swift
+++ TableSectionModules/BaseViewController.swift
@@ -12,7 +12,7 @@ import Foundation
 public class BaseViewController: UIViewController {
     
     @IBOutlet public var baseTableView:UITableView?
-    private(set) public var tableSectionModules:[TableSectionModule]?
+    private(set) public var tableSectionModules:[TableSectionModule] = []
     
     
     public override func viewDidLoad() {
@@ -25,7 +25,6 @@ public class BaseViewController: UIViewController {
     }
     
     public func configureTableSectionModules() {
-        self.tableSectionModules = []
     }
     
 }
@@ -33,79 +32,83 @@ public class BaseViewController: UIViewController {
 extension BaseViewController: TableSectionModuleSectionSource {
     public func appendModule(module: TableSectionModule) {
         module.sectionSource = self;
-        self.tableSectionModules?.append(module)
+        self.tableSectionModules.append(module)
     }
     
     public func insertModule(module: TableSectionModule, atIndex: Int) {
         module.sectionSource = self;
-        self.tableSectionModules?.insert(module, atIndex: atIndex)
+        self.tableSectionModules.insert(module, atIndex: atIndex)
     }
     
     public func removeAllModules() {
-        self.tableSectionModules?.removeAll()
+        self.tableSectionModules.removeAll()
     }
     
     public func removeModuleAtIndex(atIndex: Int) {
-        self.tableSectionModules?.removeAtIndex(atIndex)
+        self.tableSectionModules.removeAtIndex(atIndex)
     }
     
     public func removeFirstModule() {
-        self.tableSectionModules?.removeFirst()
+        self.tableSectionModules.removeFirst()
     }
     
     public func removeLastModule() {
-        self.tableSectionModules?.removeLast()
+        self.tableSectionModules.removeLast()
+    }
+    
+    public func replaceModuleAtSection(section: NSInteger, withModule module: TableSectionModule) {
+        self.tableSectionModules[section] = module
     }
     
     public func sectionForModule(module: TableSectionModule) -> NSInteger {
-        return (self.tableSectionModules?.indexOf(module))!
+        return (self.tableSectionModules.indexOf(module))!
     }
 }
 
 extension BaseViewController: UITableViewDelegate, UITableViewDataSource {
     public func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return self.tableSectionModules![section].heightForFooter()
+        return self.tableSectionModules[section].heightForFooter()
     }
     
     public func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        return self.tableSectionModules![section].viewForFooter()
+        return self.tableSectionModules[section].viewForFooter()
     }
     
     public func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return self.tableSectionModules![section].heightForHeader()
+        return self.tableSectionModules[section].heightForHeader()
     }
     
     public func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        return self.tableSectionModules![section].viewForHeader()
+        return self.tableSectionModules[section].viewForHeader()
     }
     
     public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return self.tableSectionModules!.count
+        return self.tableSectionModules.count
     }
     
     public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.tableSectionModules![section].numberOfRows()
+        return self.tableSectionModules[section].numberOfRows()
     }
     
     public func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return self.tableSectionModules![indexPath.section].heightForRow(indexPath.row)
+        return self.tableSectionModules[indexPath.section].heightForRow(indexPath.row)
     }
     
     public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return self.tableSectionModules![indexPath.section].cellForRow(indexPath.row)
+        return self.tableSectionModules[indexPath.section].cellForRow(indexPath.row)
     }
     
     public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        self.tableSectionModules![indexPath.section].didSelectRow(indexPath.row)
+        self.tableSectionModules[indexPath.section].didSelectRow(indexPath.row)
     }
     
     public func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return self.tableSectionModules![indexPath.section].canEditRow(indexPath.row)
+        return self.tableSectionModules[indexPath.section].canEditRow(indexPath.row)
     }
     
     public func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        self.tableSectionModules![indexPath.section].commitEditingStyle(editingStyle, forRow: indexPath.row)
+        self.tableSectionModules[indexPath.section].commitEditingStyle(editingStyle, forRow: indexPath.row)
     }
     
 }
